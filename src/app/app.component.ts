86246522e549c2ccd946d83b89905286f4061a30
Michael-Xie <xiemicha@gmail.com>
2020-05-12 01:12:14 -0400

    Initial commit with dynamic strike-through of done tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crash-todolist';
+  name: string = 'Brad';
+
+  constructor() {
+    console.log(123);
+    this.changeName("Ted");
+  }
+
+  changeName(name: string) {
+    this.name = name;
+  }
 }
